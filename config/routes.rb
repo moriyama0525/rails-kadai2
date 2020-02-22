8bae5054749533ffce3ff76d9b410caa313960cc
takamasa moriyama <ymca0525a@gmail.com>
2020-02-22 01:35:21 +0000

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root to: "users#index"
-    get "/new", to:"users#new"
+    get "/users/new", to:"users#new"
     post "/create", to:"users#create"
     get "/user/:id/edit", to:"users#edit"
     patch "/user/:id", to:"users#update"
